ca0b95614bcdd113e0e730b980adc4e4057a7e23
Nael Shaban <nael.shaban+github@gmail.com>
2016-05-09 05:44:13 +0300

    Update settings and requirements to support CI server

--- readit/settings.py
+++ readit/settings.py
@@ -92,6 +92,18 @@ if DJANGO_MODE == 'local':
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
+elif DJANGO_MODE == 'staging':
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.getenv("DB_NAME"),
+            'USER': os.getenv("DB_USER"),
+            'PASSWORD': os.getenv("DB_PASSWORD"),
+            'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+            'PORT': os.getenv('DB_PORT', '5432')
+        }
+    }
+
 
 
 # Password validation
